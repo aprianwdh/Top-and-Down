798fb1b21f69f635e63b855f5c2b9a6146f3e72a
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-11-20 22:12:12 +0700

    menjalankan animasi dengan animation tree
    20-11-24

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,11 +1,38 @@
 extends CharacterBody2D
 
 const SPEED = 5000
+@onready var animation_tree = $AnimationTree
+var atacking = false
 
 func _physics_process(delta):
-	var vector_x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
-	var vector_y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
-	
-	self.velocity = Vector2(vector_x,vector_y)*delta*SPEED
-	
-	move_and_slide()
+	if Input.is_action_just_pressed("ui_accept"):
+		animation_tree.get('parameters/playback').travel('atack')
+		atacking = true
+		
+	if atacking == false:
+		var input_vector = Vector2(Input.get_axis('ui_left','ui_right'),Input.get_axis('ui_up','ui_down')).normalized()
+		
+		self.velocity = input_vector*delta*SPEED
+		
+		if input_vector == Vector2.ZERO:
+			animation_tree.get('parameters/playback').travel('iddle')
+		else :
+			animation_tree.get('parameters/playback').travel('walk')
+			animation_tree.set('parameters/atack/BlendSpace2D/blend_position',input_vector)
+			animation_tree.set('parameters/iddle/BlendSpace2D/blend_position',input_vector)
+			animation_tree.set('parameters/walk/BlendSpace2D/blend_position',input_vector)
+		move_and_slide()
+
+
+func _on_animation_tree_animation_finished(anim_name):
+	print('animated finish' + anim_name)
+	if 'atack_up' in anim_name:
+		atacking = false
+	if 'atack_down' in anim_name:
+		atacking = false
+	if 'atack_left' in anim_name:
+		atacking = false
+	if 'atack_right' in anim_name:
+		atacking = false
+
+
